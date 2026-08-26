17853734276ff2287c5594ad0ec9de62330ac881
Atta Amoah-Bediako <aamoahbe@gmail.com>
2026-08-26 01:21:43 -0300

    Moved functions to appropiate files

---
+++ github_client.py
@@ -0,0 +1,32 @@
+import httpx
+
+def get_pull_request(owner, repo, pull_number): #function returns the single pull request information
+  url = f"https://api.github.com/repos/{owner}/{repo}/pulls/{pull_number}"
+  
+  response = httpx.get(url)
+  response.raise_for_status()
+  return response.json()
+
+def get_repo(owner, repo): #function returns the repository information
+  url = f"https://api.github.com/repos/{owner}/{repo}"
+  
+  response = httpx.get(url)
+  response.raise_for_status()
+
+  return response.json()
+
+def get_changed_files(owner, repo, pull_number): #function returns the files changed in a pull request and prints the filename, status, changes, and patch for each file
+  url = f"https://api.github.com/repos/{owner}/{repo}/pulls/{pull_number}/files"
+  
+  response = httpx.get(url)
+  response.raise_for_status()
+
+  files = response.json()
+
+  for file in files: 
+    print("filename:", file["filename"])
+    print("status:", file["status"])
+    print("additions:", file["additions"])
+    print("deletions:", file["deletions"])
+    print("patch:", file.get("patch"), end="\n\n")
+
