9c69d46e075962dfa0c6d8ad33e7760938c5aea0
Atta Amoah-Bediako <aamoahbe@gmail.com>
2026-08-28 05:51:04 -0300

    Testing updated mentor questions function

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from github_client import get_pull_request, get_changed_files
-from mentor import generate_mentor_questions
+from mentor import generate_mentor_questions, generate_mentor_questions1
 
 def main():
   owner = "psf"
   repo = "requests"
-  pull_number = 7586
+  pull_number = 7543 #other tests: 7128, 7543, 7586
 
   files = get_changed_files(owner, repo, pull_number)
 
   print("Changed files:", len(files), end="\n\n")
 
   for file in files:
+      filename = file.get("filename")
       patch = file.get("patch")
 
       if patch is not None:
         filename = file.get("filename")
-        questions = generate_mentor_questions(patch)
+        questions = generate_mentor_questions1(filename, patch)
         print(f"File: {filename}")
         print(questions, end="\n\n")
 
