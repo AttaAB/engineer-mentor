acced3761dcf94076b3e7909f43cc39e550e1c6c
Atta Amoah-Bediako <aamoahbe@gmail.com>
2026-08-29 09:28:33 -0400

    Refactored

--- github_client.py
+++ github_client.py
@@ -23,3 +23,24 @@ def get_changed_files(owner, repo, pull_number): #function returns the files cha
     print("patch:", file.get("patch"), end="\n\n")
     
   return files
+
+def build_pr_context(owner, repo, pull_number): 
+  pr_context = ""
+
+  for file in changed_files:
+    filename = file.get("filename")
+    patch = file.get("patch")
+
+    if patch is not None:
+      pr_context += f"File: {filename}\n"
+      pr_context += f"Diff:\n{patch}\n\n"
+  
+  print("Pull Request Context:\n", pr_context)
+  return pr_context
+
+if __name__ == "__main__":
+  owner = "psf"
+  repo = "requests"
+  pull_number = 7128 #other tests: 7128, 7543, 7586
+
+  pr_context = build_pr_context(owner, repo, pull_number)
