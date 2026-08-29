f4b4302c09347e46e369f0f7ffbac9c4fabc8f41
Atta Amoah-Bediako <aamoahbe@gmail.com>
2026-08-29 09:27:03 -0400

    Update main.py to reflect the addition of context.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from github_client import get_pull_request, get_changed_files
-from mentor import generate_mentor_questions, generate_mentor_questions1
+from mentor import generate_mentor_questions
+from context import build_pr_context
 
+'''
 def main():
   owner = "psf"
   repo = "requests"
-  pull_number = 7543 #other tests: 7128, 7543, 7586
+  pull_number = 7128 #other tests: 7128, 7543, 7586
 
   files = get_changed_files(owner, repo, pull_number)
 
@@ -16,9 +18,28 @@ def main():
 
       if patch is not None:
         filename = file.get("filename")
-        questions = generate_mentor_questions1(filename, patch)
+        questions = generate_mentor_questions(filename, patch)
         print(f"File: {filename}")
         print(questions, end="\n\n")
+'''
+
+def main():
+  owner = "psf"
+  repo = "requests"
+  pull_number = 7586 #other tests: 7128, 7543, 7586
+
+  pr = get_pull_request(owner, repo, pull_number)
+
+  files = get_changed_files(owner, repo, pull_number)
+  context = build_pr_context(files)
+
+  print("Changed files:", len(files), end="\n\n")
+
+  pr_title = pr.get("title")
+
+  mentor_questions = generate_mentor_questions(pr_title, context)
+  print(f"Pull Request Title: {pr_title}\n")
+  print("Mentor Questions:\n", mentor_questions)
 
 if __name__ == "__main__":
     main()
