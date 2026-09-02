c090c24fc0ede0816a42542397603f4da730eaf2
Atta Amoah-Bediako <aamoahbe@gmail.com>
2026-09-02 03:54:26 -0400

    Refactored Files

--- main.py
+++ main.py
@@ -2,27 +2,6 @@ from github_client import get_pull_request, get_changed_files
 from mentor import generate_mentor_questions
 from context import build_pr_context
 
-'''
-def main():
-  owner = "psf"
-  repo = "requests"
-  pull_number = 7128 #other tests: 7128, 7543, 7586
-
-  files = get_changed_files(owner, repo, pull_number)
-
-  print("Changed files:", len(files), end="\n\n")
-
-  for file in files:
-      filename = file.get("filename")
-      patch = file.get("patch")
-
-      if patch is not None:
-        filename = file.get("filename")
-        questions = generate_mentor_questions(filename, patch)
-        print(f"File: {filename}")
-        print(questions, end="\n\n")
-'''
-
 def main():
   owner = "psf"
   repo = "requests"
--- mentor.py
+++ mentor.py
@@ -59,41 +59,6 @@ def generate_mentor_questions(pr_title, context):
   
   return response.output_text
 
-#V0 mentor questions function:  
-'''
-def generate_mentor_questionsV0(patch):
-
-  prompt = f""" 
-        You are a software engineering mentor.
-
-        Your goal is to help a developer think critically about their
-        implementation rather than giving them the solution.
-
-        Review the following code.
-
-        Ask 3 concise questions about meaningful engineering decisions.
-        Focus on things such as:
-        - data structure choices
-        - complexity
-        - maintainability
-        - error handling
-        - design trade-offs
-
-        Do not provide replacement code.
-        Do not directly tell the developer what to change.
-
-        Code:
-        {patch}
-        """
-
-  response = client.responses.create(
-    model="gpt-5.6-luna",
-    input=prompt
-  )
-
-  return response.output_text
-'''
-
 if __name__ == "__main__":
   patch = """
   def find_user(users, user_id):
